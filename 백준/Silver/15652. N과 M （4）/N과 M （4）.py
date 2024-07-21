556614384f83fbdf58b9a9aefb921a9a7df7f567
이성현 <123540125+bluegown@users.noreply.github.com>
2024-07-21 22:16:00 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,20 @@
+import sys
+def dfs():
+    if len(s) == M: # 만약 요구하는 길이가 다 된다면 
+        print(' '.join(map(str,s)))
+        return 
+    for i in range(1,N+1):
+
+        if len(s)!=0:
+            if s[-1] > i:
+             continue
+        s.append(i) # 스택에 쌓아주고
+        dfs()
+        s.pop() #만약 끝나고 돌아왔다면, 이제 더이상 가지치기를 할 필요가 없으므로 pop
+N,M = map(int,sys.stdin.readline().split())
+
+visited = [False] * (N+1)
+s = []
+
+dfs()
+
