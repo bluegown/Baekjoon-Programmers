2dc6bf9d7d0a8e526924ec85164dda7e05c560ea
bluegown <123540125+bluegown@users.noreply.github.com>
2024-09-15 21:44:41 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(input())
+d = [0] * 11
+
+d[1] = 1
+d[2] = 2
+d[3] = 4
+for i in range(4,11):
+    d[i] = d[i-1] + d[i-2] + d[i-3]
+
+for _ in range(N):
+    x = int(input())
+    print(d[x])
