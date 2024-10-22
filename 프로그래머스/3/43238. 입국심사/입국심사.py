ba476828c2ca38153b9e3219cbcbb885a0b7d34c
이성현 <123540125+bluegown@users.noreply.github.com>
2024-10-22 13:44:06 +0900

    [level 3] Title: 입국심사, Time: 1104.59 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,24 +1,23 @@
 def solution(n, times):
     answer = 0
-    times.sort() # O(NlogN)
-    
-    start = 1
+    times.sort()
+    start = 0 
     end = times[-1] * n
     
     while start <= end:
         mid = (start + end) // 2
         count = 0
-        times.sort() 
-        for i in times: 
+        for i in times:
             count += (mid // i)
-        if count >= n: # 더 많은 사람을 받을 수 있음, 시간 줄이자
-            result = mid
+        if count >= n:
             end = mid - 1
+            answer = mid
         else:
             start = mid + 1
+    
 
         
         
         
         
-    return result
+    return answer
