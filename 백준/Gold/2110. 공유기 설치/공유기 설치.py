6113c927d522dd1e92ec12f7f4ad5ed40b647fa3
이성현 <123540125+bluegown@users.noreply.github.com>
2024-10-31 17:08:24 +0900

    [Gold IV] Title: 공유기 설치, Time: 572 ms, Memory: 44596 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,28 +1,33 @@
 import sys
-def binary_search(arr):
-    start = 1
-    res = 0
-    end = arr[-1] - arr[0]
-    while start <= end:
-        mid = (start + end) // 2
-        value = arr[0]
-    
-        count = 1 
-        for i in range(1,N):
-            if arr[i] - value >= mid: # 둘 사이의 거리가 mid보다 크다면? 설치 가능
-                value = arr[i] # 실시간으로 value를 바꿔준다
-                count +=1 # 설치
-        if count >= C:# 만약 c보다 더 설치할 수 있거나 같은 경우라면 , start를 증가시켜서 mid를 더 증가시킨다
-            start = mid + 1 
-            res = mid # 바운드리에 들어올 때 res를 기록한다.
-        else: # 아니라면 mid를 감소시켜야 함
-            end = mid - 1
-    return res
-    
+# sys.stdin.readline()
+from collections import deque
+sys.setrecursionlimit(100000000)
+import heapq
+import copy
+import itertools
+from itertools import combinations
+from itertools import permutations
+INF = 1e9
+from bisect import bisect_left,bisect_right
 
 N,C = map(int,sys.stdin.readline().split())
-arr = []
-for i in range(N):
-    arr.append(int(input()))
-arr.sort() 
-print(binary_search(arr))
+array = []
+for _ in range(N):
+    array.append(int(sys.stdin.readline().rstrip()))
+array.sort()
+end = array[-1] - array[0] 
+start = 1
+while start <= end:
+    count = 1
+    mid = (start + end) // 2
+    current = array[0]
+    for i in range(1,len(array)):
+        if array[i] - current >= mid:
+            count +=1
+            current = array[i]
+    if count >= C: # 공유기가 더 설치되거나 딱 맞게 설치되는 경우, 범위를 넓혀야 한다 
+        start = mid + 1 # 범위를 넓혀야 하는 거니까 mid를 더 넓혀야 적게 설치
+        answer = mid
+    else: # 공유기가 덜 설치된 경우, 범위를 좁혀야 한다
+        end = mid - 1 # end를 줄이는 방법으로 mid를 더 좁혀야 많이 설치
+print(answer)
