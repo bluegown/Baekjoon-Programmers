9eaf858d7d210132b035b879659423ee57b6044c
이성현 <123540125+bluegown@users.noreply.github.com>
2024-11-19 14:27:34 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+
+N = int(input())
+
+queue = deque()
+
+for _ in range(N):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push_front':
+        queue.appendleft(int(command[1]))
+    
+    if command[0] == 'push_back':
+        queue.append(int(command[1]))
+    if command[0] == "pop_front":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    if command[0] == "pop_back":
+         if len(queue) == 0:
+            print(-1)
+         else:
+            print(queue.pop())
+    if command[0] == 'size':
+        print(len(queue))
+    if command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    if command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    
+    if command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
