51835144f9f917a31e28dca8ff54ecc2a54abd4e
이성현 <123540125+bluegown@users.noreply.github.com>
2025-05-06 17:42:48 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,7 +1,11 @@
 def solution(n):
     answer = 0
-    while n > 10:
-        answer += (n % 10)
-        n = n // 10
-    answer += n
+    # while n > 10:
+    #     answer += (n % 10)
+    #     n = n // 10
+    # answer += n
+    # return answer
+    for i in str(n):
+        answer += int(i)
     return answer
+        
