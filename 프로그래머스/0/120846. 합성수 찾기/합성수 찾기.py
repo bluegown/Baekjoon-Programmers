77f23d0b720662402bee56f1b085374694c7dde4
이성현 <123540125+bluegown@users.noreply.github.com>
2025-05-12 00:13:03 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -2,11 +2,10 @@ def solution(n):
     answer = 0
     
     for i in range(1,n+1): # 이래야 1부터 n까지 돌아가지
-        count = 0
-        for j in range(1, i // 2 + 1):
+        for j in range(2, int(i ** (0.5)) + 1):
             if i % j == 0:
-                count += 1
-        if count >= 2:
-            answer += 1
+                answer += 1 
+                break
+
         
     return answer
