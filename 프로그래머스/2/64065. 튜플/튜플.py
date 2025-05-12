bd08e92056e30a557573720b2ed51a6e2025da71
이성현 <123540125+bluegown@users.noreply.github.com>
2025-05-13 00:45:07 +0900

    [level 2] Title: 튜플, Time: 1052.19 ms, Memory: 18.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -0,0 +1,31 @@
+def changeToDigit(arr):
+    x = []
+    for i in range(len(arr)):
+        x.append(int(arr[i]))
+    
+    return x
+
+def solution(s):
+    answer = []
+    x = []
+    
+    for i in range(1,len(s)):
+        index = i
+        count = 0
+        if s[i] == '{':
+            while True:
+                if s[index] == '}':
+                    break
+                index += 1
+            arr = s[i+1:index].split(',')
+            x.append(arr)
+            
+    x = sorted(x, key = len)
+    for i in range(len(x)):
+        for j in range(len(x[i])):
+            if int(x[i][j]) not in set(answer):
+                answer.append(int(x[i][j]))
+    
+
+
+    return answer
