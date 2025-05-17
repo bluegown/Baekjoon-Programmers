b25a12f6a2ba210cc95c6cbd6e5adbc6cff5520f
이성현 <123540125+bluegown@users.noreply.github.com>
2025-05-17 23:23:21 +0900

    [level 0] Title: A로 B 만들기, Time: 0.36 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,19 +1,8 @@
 def solution(before, after):
     answer = 0
-    bf = {}
-    af = {}
-    for i in before:
-        if i not in bf:
-            bf[i] = 1
-        else:
-            bf[i] += 1
-    
-    for i in after:
-        if i not in af:
-            af[i] = 1
-        else:
-            af[i] += 1
-    if bf == af:
+    before = sorted(before)
+    after = sorted(after)
+    if before == after:
         answer = 1
     else:
         answer = 0
