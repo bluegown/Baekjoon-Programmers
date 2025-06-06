966694db4759bc589ef1578bc331fb7a4d631d93
이성현 <123540125+bluegown@users.noreply.github.com>
2025-06-06 21:41:29 +0900

    [level 1] Title: 모의고사, Time: 9.36 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,25 @@
 def solution(answers):
     answer = []
-    
     ans = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
-    score = [0,0,0]
-    for j in range(3): # 3명에 대해서 탐색 시작
+
+    value = []
+    for i in range(len(ans)):
         count = 0
-        for k in range(len(answers)): #이제 안에있는 배열 구석구석 탐색 시작..
-          if answers[k] == ans[j][k % len(ans[j])]:
-            count += 1
-        score[j] = count
-                
-        
-    maxValue = max(score)
-    for i in range(len(score)):
-        if maxValue == score[i]:
-            answer.append(i+1)
+        for j in range(len(answers)):
+            if i == 0:
+                index = 5
+            if i == 1:
+                index = 8
+            if i == 2:
+                index = 10
+            if answers[j] == ans[i][j % index]:
+                count += 1
+        value.append([i+1,count])
+    value = sorted(value,key = lambda x: x[1], reverse = True)
+    maxValue = -1
+    print(value)
+    for i in range(3):
+        if value[i][1] >= maxValue:
+            answer.append(value[i][0])
+            maxValue = value[i][1]
     return answer
