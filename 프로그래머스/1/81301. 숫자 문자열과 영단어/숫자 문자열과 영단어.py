c2aec158862c6ef59758f11adeca87ae42208a95
이성현 <123540125+bluegown@users.noreply.github.com>
2025-06-17 23:34:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,15 +1,5 @@
 def makeDict():
-    dic = dict()
-    dic["zero"] = 0
-    dic["one"] = 1
-    dic["two"] = 2
-    dic["three"] = 3
-    dic["four"] = 4
-    dic["five"] = 5
-    dic["six"] = 6
-    dic["seven"] = 7
-    dic["eight"] = 8
-    dic["nine"] = 9
+    dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
     
     return dic
 def solution(s):
