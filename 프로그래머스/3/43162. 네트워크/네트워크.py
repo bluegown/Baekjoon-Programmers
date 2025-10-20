de89b54e34f094e210d722351f06be9ab3b966f8
이성현 <123540125+bluegown@users.noreply.github.com>
2025-10-20 22:26:17 +0900

    [level 3] Title: 네트워크, Time: 0.67 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,26 +1,26 @@
 from collections import deque
-def bfs(computers,visited,i):
-    visited[i] = True
-    
+def bfs(computers, visited , start):
+    if visited[start] == True:
+        return 0 # false면 순회돌 필요 없지
+    visited[start] = True
     queue = deque()
-    queue.append(i)
+    queue.append(start)
     
     while queue:
-        v = queue.popleft()
-        for element in range(len(computers[v])):
-            if not visited[element] and computers[v][element] == 1:
-                queue.append(element)
-
-                visited[element] = True 
+        elem = queue.popleft()
+        for i in range(len(computers)):
+            if visited[i] == False:
+                if start!= i and computers[elem][i] == 1:
+                    queue.append(i)
+                    visited[i] = True    
+    return True
 def solution(n, computers):
     answer = 0
-
-    visited = [False] * (n)
+    visited = [False]  * (n)
+    # 1번부터 시작해서 visited가 false면 돌려돌려
     
-    for i in range(len(visited)):
-        if visited[i] == False: # 만약 방문하지 않은 노드라면? 탐색 ㄱㄱ
-            bfs(computers,visited,i)
+    for i in range(n):
+        if bfs(computers,visited,i) == True:
             answer += 1
-
         
     return answer
