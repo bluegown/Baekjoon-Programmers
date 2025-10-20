130d19693ab4a03d5af0d7b76caf6c485ec18e19
이성현 <123540125+bluegown@users.noreply.github.com>
2025-10-20 22:34:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.30 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,24 @@
 from collections import deque
 def solution(maps):
     answer = 0
+    
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
-    N = len(maps)
-    M = len(maps[0])
+    # Maps[x][y] == 1인 경우만 통과가능
     queue = deque()
     queue.append((0,0))
-    ans = [[-1 for _ in range(M)] for _ in range(N)]
-    ans[0][0] = 1
+    N = len(maps)
+    M = len(maps[0])
     while queue:
         x,y = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
+            nx = x + dx[i] 
             ny = y + dy[i]
             if nx < 0 or ny < 0 or nx >= N or ny >= M:
                 continue
-            if ans[nx][ny] == -1 and maps[nx][ny] == 1:
-                ans[nx][ny] = ans[x][y] + 1
+            if maps[nx][ny] == 1: # 통행 가능한 경우
                 queue.append((nx,ny))
-        maps[x][y] = 0
-
-            
-        
-        
-    
-    return ans[N-1][M-1]
+                maps[nx][ny] = maps[x][y] + 1
+    if maps[N-1][M-1] <= 1:
+        return -1
+    return maps[N-1][M-1]
