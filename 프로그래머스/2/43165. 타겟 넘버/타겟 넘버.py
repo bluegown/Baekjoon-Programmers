f765e8698252e2778ae3b6a71fe46840775c62c8
이성현 <123540125+bluegown@users.noreply.github.com>
2025-10-20 21:38:43 +0900

    [level 2] Title: 타겟 넘버, Time: 774.02 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,20 @@
+from collections import deque
+# 주어지는 숫자의 개수는 2~ 20
 def solution(numbers, target):
-    n = len(numbers)
     answer = 0
-    def dfs(idx, result):
-        if idx == n:
-            if result == target:
-                nonlocal answer
-                answer += 1
-            return
-        else:
-            dfs(idx+1, result+numbers[idx])
-            dfs(idx+1, result-numbers[idx])
-    dfs(0,0)
+    queue = deque()
+    queue.append((-numbers[0],0))
+    queue.append((numbers[0],0)) 
+    # queue의 첫번째 원소는 숫자, 두번째 원소는 인덱스
+    
+    while queue:
+        value, index = queue.popleft()
+        index += 1
+        if index < len(numbers):
+            queue.append((value + numbers[index], index))
+            queue.append((value - numbers[index], index)) 
+        if value == target and index == len(numbers):
+            answer += 1
+
+            
     return answer
