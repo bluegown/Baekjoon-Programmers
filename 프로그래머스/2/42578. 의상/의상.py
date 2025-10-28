0324332517b83a6aef1f26f9d04ad8cecc2cb805
이성현 <123540125+bluegown@users.noreply.github.com>
2025-10-28 22:01:25 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,8 @@
 def solution(clothes):
     answer = 1
-    dic = dict()
-    
-    for item , category in clothes:
-        dic[category] = dic.get(category,0) + 1
-
-    for key, value in dic.items():
+    ans = dict()
+    for key, value in clothes:
+        ans[value] = ans.get(value,0) + 1
+    for key, value in ans.items():
         answer = answer * (value + 1)
-
-    
     return answer - 1
