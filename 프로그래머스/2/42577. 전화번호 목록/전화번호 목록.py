25dd1934704234855bfba559cf4de2d770ee11aa
이성현 <123540125+bluegown@users.noreply.github.com>
2025-10-28 21:53:19 +0900

    [level 2] Title: 전화번호 목록, Time: 462.92 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,17 +1,12 @@
 def solution(phone_book):
     answer = True
-    book_dict = set()
-    for i in phone_book:
-        book_dict.add(i)
+    phone = set(phone_book)
     
     for i in phone_book:
         string = ''
         for j in i:
-            string += j
-            if string in book_dict and string!=i: # 접두어인 경우, 완전같으면 안됨
+            string += j # 한글자씩 더해본다
+            if string!= i and string in phone: # 만약 포함되어있다면?
                 return False
-
-    
-    
-    
+        
     return True
