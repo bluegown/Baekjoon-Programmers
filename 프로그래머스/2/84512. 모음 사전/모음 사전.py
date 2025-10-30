ef0ab234ed46ae622d8af930342dedac7f79d127
이성현 <123540125+bluegown@users.noreply.github.com>
2025-10-30 23:16:27 +0900

    [level 2] Title: 모음 사전, Time: 11.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,13 +1,13 @@
-def makeWord(ans, value, length):
+def makeWord(ans , value, length):
+    arr = ['A','E','I','O','U']
     if length == 6:
-        return
+        return 
     if value != '':
         ans.append(value)
-    for i in ['A','E','I','O','U']:
-        makeWord(ans, ''.join([value,i]), length + 1)
-    
+    for i in arr:
+        makeWord(ans,''.join([value,i]),length + 1)
 def solution(word):
     answer = 0
     ans = []
-    makeWord(ans, '', 0) 
+    makeWord(ans,'',0)
     return ans.index(word) + 1
