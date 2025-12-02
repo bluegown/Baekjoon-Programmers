335152806734867fda5796bb1504bd2c4f35b107
이성현 <123540125+bluegown@users.noreply.github.com>
2025-12-02 21:16:52 +0900

    [level 2] Title: 타겟 넘버, Time: 676.88 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,20 @@
 from collections import deque
-# 주어지는 숫자의 개수는 2~ 20
 def solution(numbers, target):
     answer = 0
     queue = deque()
-    queue.append((-numbers[0],0))
-    queue.append((numbers[0],0)) 
-    # queue의 첫번째 원소는 숫자, 두번째 원소는 인덱스
-    
+    queue.append((-numbers[0] , 0))
+    queue.append((numbers[0] , 0))
+    sumValue = 0
     while queue:
-        value, index = queue.popleft()
+        value , index = queue.popleft()
         index += 1
         if index < len(numbers):
-            queue.append((value + numbers[index], index))
-            queue.append((value - numbers[index], index)) 
+            queue.append((-numbers[index] + value, index))
+            queue.append((numbers[index] + value , index))
         if value == target and index == len(numbers):
             answer += 1
-
-            
+        
+        
+        
+    
     return answer
