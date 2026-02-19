369416cad1692505d75266faef3880980b7db02a
이성현 <123540125+bluegown@users.noreply.github.com>
2026-02-19 20:51:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.44 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            if (arr[i-1] == arr[i]){
+                continue;
+            }else{
+                list.add(arr[i]);
+            }
+        }
+        
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
