3ede21c681d7ef6d555da96ab1f59d92097af87b
이성현 <123540125+bluegown@users.noreply.github.com>
2026-03-17 23:32:10 +0900

    [Silver IV] Title: 베스트셀러, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,18 @@
+import sys
+
+answer = 0
+N = int(input())
+d = dict()
+
+for _ in range(N):
+    i = input()
+    d[i] = d.get(i,0) + 1
+
+result = sorted(d.items(), key = lambda x: (-x[1],x[0]))
+
+print(result[0][0])
+
+
+
+    
+
