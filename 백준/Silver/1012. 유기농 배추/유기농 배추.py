f9f65723ffdd1add655207d9689488474b50d484
이성현 <123540125+bluegown@users.noreply.github.com>
2026-03-29 21:25:56 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,43 +1,42 @@
 import sys
 from collections import deque
 
-def bfs(x,y):
-     visited[x][y] = True
-     queue = deque()
-     queue.append((x,y))
-     dx = [-1,0,1,0]
-     dy = [0,1,0,-1]
-     while queue:
-      a,b = queue.popleft()
-      for i in range(4):
-        nx = a + dx[i]
-        ny = b + dy[i]
-        if nx < 0 or ny < 0 or nx>=M or ny>=N:
-            continue
-        if graph[nx][ny] == 1 and visited[nx][ny] == False:
-         visited[nx][ny] = True
-         queue.append((nx,ny))
-    
-               
+def bfs(graph, a, b):
+    queue = deque()
+    queue.append((a,b))
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= M or ny >=N:
+                continue
+            if graph[nx][ny] == 1:
+                queue.append((nx,ny))
+                graph[nx][ny] = 0
 
-    
+        
+        
 
 T = int(input())
+
 for _ in range(T):
+    answer = 0
     M,N,K = map(int,sys.stdin.readline().split())
     graph = [[0 for _ in range(N)] for _ in range(M)]
-    visited = [[False for _ in range(N)] for _ in range(M)]
-    for _ in range(K):
-        x,y = map(int,sys.stdin.readline().split())
-        graph[x][y] = 1
-    count = 0
+    for i in range(K):
+        a,b = map(int,sys.stdin.readline().split())
+        graph[a][b] = 1
+    for i in range(len(graph)):
+        for j in range(len(graph[i])):
+            if graph[i][j] == 1:
+                bfs(graph,i,j)
+                answer += 1
+                
+    print(answer)
+        
 
-    for i in range(M):
-        for j in range(N):
-            if graph[i][j] == 1 and visited[i][j] == False:
-                 bfs(i,j)
-                 count +=1
-    print(count)
-
-
-
+    
+        
