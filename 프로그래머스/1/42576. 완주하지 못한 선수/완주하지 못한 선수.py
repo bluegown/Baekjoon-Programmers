ab3d3d35ddec956dc7650119e541dc3ead6ff044
이성현 <123540125+bluegown@users.noreply.github.com>
2026-04-29 00:48:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.77 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,11 @@
 def solution(participant, completion):
     answer = ''
     ans = dict()
-    
     for i in participant:
         ans[i] = ans.get(i,0) + 1
     for i in completion:
         ans[i] -= 1
-    for index, value in ans.items():
+    for key, value in ans.items():
         if value == 1:
-            answer = index
+            return key
     return answer
