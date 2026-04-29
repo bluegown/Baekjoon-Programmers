da84a22fbb97e70d9fa9ed06ef5e81623c4e3b9b
이성현 <123540125+bluegown@users.noreply.github.com>
2026-04-30 01:22:10 +0900

    [level 2] Title: 전화번호 목록, Time: 456.01 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,13 @@
 def solution(phone_book):
     answer = True
     
-    s = set(phone_book)
+    set_pb = set(phone_book)
     
     for i in phone_book:
-        string = ''
+        st = ''
         for j in i:
-            string += j
-            if string in s and string!=i:
-                return False
-            
-    
+            st += j
+            if st != i and st in set_pb:
+                return False # 본인과 같은 원소가 아니고, 현재까지 자른 string이 set()에 존재
+        
     return answer
