47b640f1842853e0dbc3ecd9d88a82e0b9d8d7c1
이성현 <123540125+bluegown@users.noreply.github.com>
2026-04-30 01:14:51 +0900

    [level 1] Title: 폰켓몬, Time: 2.07 ms, Memory: 9.76 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,14 +1,13 @@
 def solution(nums):
     answer = 0
-    
-    num = len(nums) // 2 # max로 가질수 있는 숫자
-    ans = set(nums)
-    if len(ans) <= num:
-        return len(ans)
-    else:
+    arr = dict()
+    for i in nums:
+        arr[i] = arr.get(i,0) + 1
+    print(arr)
+    if len(arr) == len(nums) // 2: # 딱 N / 2개의 종류가 있는 경우
+        return len(arr)
+    elif len(arr) > len(nums) // 2: # 4마리 포켓몬이 있는데 3가지 종류 가지고있는 경우
         return len(nums) // 2
-    
-
-
-
+    else:
+        return len(arr)
     return answer
