b96c4d55df055b540cc2e759960be0a5de7a659c
이성현 <123540125+bluegown@users.noreply.github.com>
2026-05-08 00:52:49 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 9.47 MB, 시간: 0.04 ms
+메모리: 9.29 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,11 +16,11 @@
 
 ### 제출 일자
 
-2025년 06월 17일 23:34:36
+2026년 05월 08일 00:52:45
 
 ### 문제 설명
 
-<p><img src="https://grepp-programmers.s3.ap-northeast-2.amazonaws.com/files/production/d31cb063-4025-4412-8cbc-6ac6909cf93e/img1.png" title="" alt="img1.png"></p>
+<p><img src="https://asset.programmers.co.kr/files/production/d31cb063-4025-4412-8cbc-6ac6909cf93e/img1.png" title="" alt="img1.png"></p>
 
 <p>네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.<br><br>
 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.</p>
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,21 +1,16 @@
-def makeDict():
-    dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
-    
-    return dic
 def solution(s):
-    answer = ""
-    string = ""
-    
-    dic = makeDict()
-    
+    answer = ''
+    dic = {"zero" : 0 , "one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, 
+          "six" : 6, "seven" : 7, "eight" : 8, "nine" : 9}
+    string = ''
     for i in range(len(s)):
-        string += s[i]
+        if s[i].isdigit():
+            answer += s[i]
+        else:
+            string += s[i] # 하나씩 쌓아간다
         if string in dic:
             answer += str(dic[string])
-            string = ""
-        elif string.isdigit():
-            answer += string
-            string = ""
+            string = ''
+    
             
-        
     return int(answer)
