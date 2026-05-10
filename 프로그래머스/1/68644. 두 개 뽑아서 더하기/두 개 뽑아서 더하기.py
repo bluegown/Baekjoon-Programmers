15de466c2385a12d30ab89e6eb7a5720ed0f3f12
이성현 <123540125+bluegown@users.noreply.github.com>
2026-05-10 23:38:08 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.72 ms, Memory: 9.01 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,9 +1,12 @@
 from itertools import combinations
 def solution(numbers):
     answer = []
-    co = list(combinations(numbers,2))
-    for i in co:
-        if i[0] + i[1] not in answer:
-            answer.append(i[0] + i[1])
-    answer.sort()
+    arr = list(combinations(numbers, 2))
+    res = set()
+    
+    for i in arr:
+        x = i[0] + i[1]
+        if x not in res:
+            res.add(x)
+    answer = sorted(res)
     return answer
