d7ae22223f63570bc3120d93f969bc9d3697f602
이성현 <123540125+bluegown@users.noreply.github.com>
2026-05-17 22:42:41 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.44 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 9.35 MB, 시간: 0.30 ms
+메모리: 9.21 MB, 시간: 0.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 30일 16:54:33
+2026년 05월 17일 22:42:37
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,46 +1,35 @@
 def solution(numbers, hand):
-    answer = ''
-    left_row, left_col , right_row, right_col = 3,0,3,2
-    # left, right 변수는 모두 row, col 기준으로 사용
-    arr = [[1,2,3],[4,5,6],[7,8,9],['*',0,'#']]
+    result = ''
+    left_number = {1,4,7}
+    right_number = {3,6,9}
+    left = [3,0]
+    right= [3,2] # 행 / 열
     
-    for elements in numbers:
-        if elements in [1,4,7]: # [1,4,7]
-            left_row = elements // 3 # 0행 , 1행 , 2행
-            left_col = 0
-            answer += 'L' # 무조건 왼손으로 움직인다
-        elif elements in [3,6,9]:  #[3,6,9]
-            right_row = (elements -1) // 3 # 0행, 1행, 2행
-            right_col = 2
-            answer += 'R' # 무조건 오른손으로 움직임
-        else: # 왼손 vs 오른쪽 위치를 판별해야 하는 경우
-            now_col = 1 # 얘는 가운데니까 1행으로 고정
-            if elements in [2,5,8]: # [2,5,8]0열, 1열, 2열
-                now_row = (elements) // 3
-            elif elements == 0:
-                now_row = 3 # 만약 0인 경우는 3열 . 이렇게 현재 위치 지정 완료
-                
-            left_distance = abs(left_col - now_col) + abs(left_row - now_row)
-            right_distance = abs(right_col - now_col) + abs(right_row - now_row)
-            if right_distance > left_distance: # 만약 오른쪽 거리가 왼쪽보다 적은 경우 -> 왼손 사용
-                left_col = now_col
-                left_row = now_row
-                answer += 'L'
-            elif right_distance < left_distance: # 만약 왼손 거리가 오른쪽보다 적은 경우 -> 오른손 사용
-                right_col = now_col
-                right_row = now_row
-                answer += 'R'
-            else: # 만약 둘의 길이가 같은 경우
+    for i in range(len(numbers)):
+        if  numbers[i] in left_number:
+            result += 'L'
+            left = [(numbers[i] - 1) // 3 , 0]
+        elif numbers[i] in right_number:
+            result += 'R'
+            right = [(numbers[i]  - 3) // 3 , 2]
+        else:
+            # 2,5,8,0에 위치한 경우, col은 1로 고정
+            if numbers[i]  == 0:
+                row = 3
+            else:
+                row = (numbers[i]  - 2) // 3 
+            col = 1
+            if abs(left[0] - row) + abs(left[1] - col) < abs(right[0] - row) + abs(right[1] - col):
+                result += 'L'
+                left = [row, col]
+            elif abs(left[0] - row) + abs(left[1] - col) > abs(right[0] - row) + abs(right[1] - col):
+                result += 'R'
+                right = [row, col]
+            else:
                 if hand == 'right':
-                    right_col = now_col
-                    right_row = now_row
-                    answer += 'R'
+                    result += 'R'
+                    right = [row, col]
                 else:
-                    left_col = now_col
-                    left_row = now_row
-                    answer += 'L'
-                
-            
-            
-            
-    return answer
+                    result += 'L'
+                    left = [row, col]
+    return result
