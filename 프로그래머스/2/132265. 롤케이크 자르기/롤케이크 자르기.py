2b5a114c0e2427b71ebf61989fb27a9a3cd0a877
이성현 <123540125+bluegown@users.noreply.github.com>
2026-05-21 00:16:51 +0900

    [level 2] Title: 롤케이크 자르기, Time: 325.76 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,20 +1,15 @@
 def solution(topping):
     answer = 0
-    s = dict()
-    
-    for i in topping:
-        s[i] = s.get(i,0) + 1
-    
+    s1 = dict()
     s2 = dict()
-    
     for i in topping:
-        s[i] -= 1 # 수 하나 줄이고
-        if s[i] == 0:
-            del s[i]
-            
+        s1[i] = s1.get(i, 0 ) + 1
+    for i in topping:
+        s1[i] -= 1
+        if s1[i] == 0:
+            del s1[i]
+        
         s2[i] = s2.get(i,0) + 1
-        if len(s) == len(s2):
+        if len(s1) == len(s2):
             answer += 1
-            
-
     return answer
