5683845ccc5a240424befc9890463b80af8f9cdb
이성현 <123540125+bluegown@users.noreply.github.com>
2026-06-02 20:49:53 +0900

    [level 2] Title: 타겟 넘버, Time: 276.41 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,18 @@
 from collections import deque
 def solution(numbers, target):
     answer = 0
-    queue = deque()
-    queue.append((-numbers[0] , 0))
-    queue.append((numbers[0] , 0))
-    sumValue = 0
-    while queue:
-        value , index = queue.popleft()
-        index += 1
-        if index < len(numbers):
-            queue.append((-numbers[index] + value, index))
-            queue.append((numbers[index] + value , index))
-        if value == target and index == len(numbers):
-            answer += 1
-        
-        
-        
     
+    queue = deque()
+    queue.append((numbers[0], 0))
+    queue.append((-numbers[0], 0))
+    
+    while queue:
+        value, idx = queue.popleft() #숫자 , 몇번째인지 카운트
+        idx += 1
+        if idx < len(numbers): # 아직 연산이 다 안되었다면?
+            queue.append((value + numbers[idx], idx ))
+            queue.append((value - numbers[idx], idx ))
+        else:
+            if value == target:
+                answer += 1
     return answer
