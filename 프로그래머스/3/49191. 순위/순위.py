aa2688f4da6a615b65d2bbca874e6d4710eee800
이성현 <123540125+bluegown@users.noreply.github.com>
2026-06-09 23:57:45 +0900

    [level 3] Title: 순위, Time: 66.05 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,23 +1,26 @@
 def solution(n, results):
     answer = 0
+    # n이 100 이하 > 플로이드 워셜 알고리즘 사용!
+    
     graph = [['X' for _ in range(n)] for _ in range(n)]
+    
     for i in range(n):
-        graph[i][i] = 0
+        for j in range(n):
+            if i == j:
+                graph[i][j] = 0 # 대각선 원소는 0으로 초기화한다.
+    for a,b in results:
+        graph[a-1][b-1] = 1 # 승리
+        graph[b-1][a-1] = -1 # 패배
     
-    for x,y in results:
-        graph[x-1][y-1] = 1
-        graph[y-1][x-1] = -1
-    
-     
     for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if graph[i][k] == 1 and graph[k][j] == 1:
-                    graph[i][j] = 1
-                elif graph[i][k] == -1 and graph[k][j] == -1:
-                    graph[i][j] = -1
+        for a in range(n): # A > K > B
+            for b in range(n):
+                if graph[a][k] == 1 and graph[k][b] == 1:
+                    graph[a][b] = 1
+                elif graph[a][k] == -1 and graph[k][b] == -1:
+                    graph[a][b] = -1
+    print(graph)
     for i in graph:
         if 'X' not in i:
             answer += 1
-            
     return answer
