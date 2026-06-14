5dea08a868d2b8adff3482805388f1fdc8d27f5d
이성현 <123540125+bluegown@users.noreply.github.com>
2026-06-14 23:33:31 +0900

    [level 3] Title: N으로 표현, Time: 6.47 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,21 +1,20 @@
 def solution(N, number):
-    dp = [set() for _ in range(9)]
+    answer = -1
+    dp = [set() for _ in range(9)] # N의 범위가 1부터 9 사이에 위치
     
-    for i in range(1, 9): # N의 범위
-        arr = dp[i] # 계산 횟수당 사용할 수 있는지를 검사하는 arr
-        arr.add(int(str(N) * i)) # 555, 55와 같은 형태로 사용됨
+    for i in range(1, 9):
+        arr = dp[i]
+        arr.add(int(str(N) * i)) # 555, 55와 같음
         
-        for j in range(1, i):
-            for k in dp[j]: # 현재까지 계산해온 dp배열 내 기록된 수들에 대해서
+        for j in range(1, i ):
+            for k in dp[j]: # 현재까지 계산해온 dp배열에 대해 확인
                 for m in dp[i - j]:
                     arr.add(k + m)
                     arr.add(k - m)
                     arr.add(k * m)
-                    if k!= 0 and m!=0:
-                        arr.add( k // m)
-        
+                    if m!= 0 and k!=0:
+                        arr.add(k // m)
         if number in arr:
             return i
     
-    return -1
-                    
+    return answer
