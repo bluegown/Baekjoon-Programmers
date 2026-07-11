1eccea76ee1d6c84e50a750a1f9e1e6b5118c6d2
이성현 <123540125+bluegown@users.noreply.github.com>
2026-07-11 22:48:47 +0900

    [level 1] Title: 공원 산책, Time: 0.48 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,67 +1,44 @@
-def chkoutIndex(nx , ny):
-    if nx < 0 or ny < 0 or nx >= rows or ny >= cols:
-        return True
-    return False
 def solution(park, routes):
     answer = []
-    global rows #row 는 세로 
-    global cols  #col은 가로
-    rows = len(park)
-    cols = len(park[0]) 
-    for i in range(rows):
-        for j in range(cols):
-            if park[i][j] == 'S':
-                row, col  = i,j # 시작점 설정하기
-
-
-
+    # 1. 주어진 방향으로 이동시 공원을 벗어나는지
+    # 2. 이동 중 장애물을 만나는지 확인
+    graph = [[] for _ in range(len(park))]
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            graph[i].append(park[i][j])
+    for i in range(len(graph)):
+        for j in range(len(graph[0])):
+            if graph[i][j] == 'S':
+                x,y = i,j # 시작점 세팅
+    # N, S , W, E (세로, 가로)
+    # 북쪽으로 한칸. 세로 한칸 차감
+    # 남쪽으로 한칸, 세로 한칸 추가
+    # 서쪽으로 한칸, 가로 한칸 차감
+    # 동쪽으로 한칸, 가로 한칸 추가
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
+    # S, N, E, W
+    # 1. 벽 검사
+    # 2. 장애물 검사
+    N = len(graph)
+    M = len(graph[0])
+    way = {'N' : 0, 'E' : 1, 'S' : 2, 'W' : 3}
     for i in routes:
-        direction, move =  i.split()
-        move = int(move)
-        chk = True
-        next_row = row
-        next_col = col
-        if direction == 'E': # 동쪽
-            next_col = col + move
-            if chkoutIndex(row, next_col) == True:
-                chk = False
-                continue
-            for j in range(col, next_col + 1):
-                if park[row][j] == 'X':
-                    chk = False
-                    break
-        elif direction == 'S':
-            next_row = row + move
-            if chkoutIndex(next_row, col) == True:
-                chk = False
-                continue
-            for j in range(row, next_row + 1):
-                if park[j][col] == 'X':
-                    chk = False
-                    break
-        elif direction == 'W':
-            next_col = col - move
-            if chkoutIndex(row,next_col ) == True:
-                chk = False
-                continue
-            for j in range(next_col , col + 1):
-                if park[row][j] == 'X':
-                    chk = False
-                    break
-        else:
-            next_row = row - move
-            if chkoutIndex(next_row, col) == True:
-                chk = False
-                continue
-            for j in range(next_row, row + 1):
-                if park[j][col] == 'X':
-                    chk = False
-                    break
-        if chk == False:
+        dr, cnt = i.split()
+        flag = False
+        nx = x + dx[way[dr]] * int(cnt)
+        ny = y + dy[way[dr]] * int(cnt)
+        if nx >= N or ny >= M or nx < 0 or ny < 0:
             continue
-        col = next_col
-        row = next_row
-    return [row,col]
-            
-        
-    
+        nx,ny = x,y
+        for _ in range(int(cnt)):
+            nx = nx + dx[way[dr]]
+            ny = ny + dy[way[dr]]
+            print(nx,ny , graph[nx][ny], way[dr])
+            if graph[nx][ny] == 'X':
+                flag = True
+                break
+        if flag:
+            continue
+        x,y = nx,ny
+    return [x,y]
