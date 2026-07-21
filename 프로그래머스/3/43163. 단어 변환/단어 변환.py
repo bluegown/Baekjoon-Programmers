21e2d0d8565b1bb2ae6d26613dc2bb7130fcf6fe
이성현 <123540125+bluegown@users.noreply.github.com>
2026-07-21 22:02:53 +0900

    [level 3] Title: 단어 변환, Time: 27.58 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,21 +1,26 @@
 from collections import deque
-
+def find_next(begin, target):
+    cnt = 0
+    for i in range(len(begin)):
+        if begin[i] != target[i]:
+            cnt += 1 # 한번까지 다름 허용
+        if cnt >= 2:
+            return False
+    return True
 def solution(begin, target, words):
     answer = 0
+    queue = deque()
+    count = 0
     if target not in words:
         return 0
-    queue = deque()
     queue.append((begin, 0))
     while queue:
-        element, answer = queue.popleft()
-        if element == target:
-            return answer
-        for i in range(len(words)):
-            count = 0
-            for j in range(len(words[0])):
-                if element[j] == words[i][j]:
-                    count += 1
-            if count == len(words[0]) - 1:
-                queue.append((words[i],answer + 1))
+        v , count = queue.popleft()
+        for word in words:
+            if find_next(v, word):
+                if v == target:
+                    return count
+                queue.append((word, count + 1))
             
-    return answer
+    return count   
+        
