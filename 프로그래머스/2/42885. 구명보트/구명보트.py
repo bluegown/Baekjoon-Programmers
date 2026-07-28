90eb220b6822c6f4ccafb0d822fe3174fe5df4de
이성현 <123540125+bluegown@users.noreply.github.com>
2026-07-29 01:05:40 +0900

    [level 2] Title: 구명보트, Time: 9.84 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,18 +1,16 @@
 def solution(people, limit):
     answer = 0
     people.sort()
-    count = 0 # 최대 인원 수 
-    weight = 0
-    index = 0
-
     start = 0
-    end = len(people) - 1
-    while start <= end:
-        if people[start] + people[end] <= limit:
+    end= len(people) - 1 # 시작과 끝 인덱스를 담는다.
+    
+    while start < end:
+        if people[start]  + people[end] <= limit:
             start += 1
-            end -= 1
-        else:
-            end -= 1
-        answer += 1
+            answer += 1
+        end -= 1
+     
 
-    return answer
+    
+    
+    return len(people) - answer
