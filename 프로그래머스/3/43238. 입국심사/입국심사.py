f67ddb0ec275e4f36af4ddc66a36920e100bece5
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-02 17:26:24 +0900

    [level 3] Title: 입국심사, Time: 419.98 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,23 +1,18 @@
-def binary_search(n , times):
-    start = 1
-    times.sort()
-    end = times[-1] * n # 시간이 기준이 된다!!
-    
-    
-    while start <= end:
-        mid = (start + end) // 2 
-        count = 0
-        for i in times:
-            count += (mid // i) # 인당 평가할 수 있는 시간 
-        if count < n: # 모든 사람 평가하지 못하는 경우
-            start = mid + 1
-        else: # 여유롭게 평가 가능함
-            end = mid - 1
-            ans = mid
-        
-    return ans   
-
 def solution(n, times):
     answer = 0
+    times.sort()
+    start = 1
+    end = times[-1] * n
     
-    return binary_search(n,times)
+    while start <= end:
+        mid = (start + end) // 2
+        total = 0
+        for i in times:
+            total += (mid // i)
+        if total >= n: # 모든 사람이 심사 가능함(필요 이상의 인원이 심사 가능한 경우)
+            end = mid - 1
+            answer = mid
+        else:
+            start = mid + 1
+            
+    return answer
