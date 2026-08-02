6e1fa321a9e9627f1c59b6ce8123902965e83dde
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-02 16:35:44 +0900

    [level 2] Title: 큰 수 만들기, Time: 39.63 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,9 +1,10 @@
 def solution(number, k):
-    answer = []
-    
+    answer = ''
+    arr = []
     for num in number:
-        while k > 0 and answer and answer[-1] < num: # 스택 맨 위원소가 number[i]보다 작다면?
+        while k > 0 and arr and arr[-1] < num:
             k -= 1
-            answer.pop() # 남아있을 자격이 없으니까 쫓아내
-        answer.append(num)
-    return ''.join(answer[:len(answer) - k ])
+            arr.pop()
+        arr.append(num)
+        
+    return ''.join(arr[:len(arr) - k])
