260dd566ba857c78fa9be07a8da4095c5717d5f7
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-02 16:21:50 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,19 +1,25 @@
+import copy
 def solution(n, lost, reserve):
-    # 정렬
-    lost.sort()
-    reserve.sort()
-	
-    # lost, reserve에 공통으로 있는 요소 제거
-    for i in reserve[:]:
-        if i in lost:
-            reserve.remove(i)
-            lost.remove(i)
-	
-    # 체육복 빌려주기(나의 앞 번호부터 확인)
-    for i in reserve:
-        if i-1 in lost:
-            lost.remove(i-1)
-        elif i+1 in lost:
-            lost.remove(i+1)
+    answer = 0
     
-    return n-len(lost)
+    reserve.sort()
+    lost.sort()
+    rs = copy.deepcopy(reserve)
+    for i in rs:
+        if i in lost: # 여벌 체육복을 가져온 학생이 체육복을 도난당한 경우
+            lost.remove(i)
+            reserve.remove(i)
+            
+    for i in reserve:
+        if i-1 in lost: # 에있는 학생에게 빌려주는 경우
+            lost.remove(i - 1)
+        elif i + 1 in lost:
+            lost.remove(i + 1)
+
+    
+            
+    
+        
+        
+            
+    return n - len(lost)
