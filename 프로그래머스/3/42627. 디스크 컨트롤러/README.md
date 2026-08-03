9ff5fd741dac4d5e8cf43ebc457c92591a409167
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-04 01:27:46 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 9.33 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=python3) 
 
 ### 성능 요약
 
-메모리: 69.3 MB, 시간: 3.15 ms
+메모리: 11.4 MB, 시간: 9.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 07일 09:40:21
+2026년 08월 04일 01:27:42
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,23 +1,25 @@
 import heapq
-from collections import deque
 def solution(jobs):
     answer = 0
-    i = 0
-    start = -1
-    now = 0
-    heap = []
-    while i < len(jobs):
-        for j in jobs:
-            if start < j[0] <= now: # 요청시점이 처리 가능하다면
-                heapq.heappush(heap,[j[1],j[0]])
-        if heap:
-            l, s = heapq.heappop(heap) # l은 작업 소요시간 , s는 요청시점
-            start = now
-            now += l
-            answer += (now - s)
-            i += 1
+    start, now = -1, 0
+    count = 0
+    q = []
+    while count < len(jobs):
+        for j in range(len(jobs)):
+            if start < jobs[j][0] <= now:
+                heapq.heappush(q,(jobs[j][1], jobs[j][0])) # 처리대상에 넣고
+        # 한바퀴를 다 돌았다.
+        # 우선순위가 될거를 앞에다가 놔주면 된다
+        if q:
+            take_time, start_time = heapq.heappop(q) # 처리 대상 시작시간 / 처리 소요시간
+            start = now # 처리할 원소를 최신화해준다
+            now += take_time
+            answer += (now - start_time)
+            count += 1
         else:
             now += 1
-
-
+            
+                
+        
+        
     return answer // len(jobs)
