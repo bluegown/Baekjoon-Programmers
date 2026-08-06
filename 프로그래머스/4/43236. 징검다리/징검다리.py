d6fe22e7b6803e7a6a9c82dd02b809139627761f
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-06 20:07:25 +0900

    [level 4] Title: 징검다리, Time: 66.04 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,25 +1,30 @@
 def solution(distance, rocks, n):
     answer = 0
-    start , end = 0, distance
-    rocks.append(distance)
     rocks.sort()
+    start = 0
+    end = distance # 제거해야 하는 바위의 개수를 기점으로 판단해보자
     
     while start <= end:
-        mid = (start + end) // 2
-        deleted_stone = 0
+        mid = (start + end) // 2 # 거리를 기준으로 삼으라고 문제에서 던져준다
         prev_stone = 0
-        for idx in rocks:
-            if idx - prev_stone < mid: # 만약 이전 돌과의 거리가 mid보다 작다면 제거
-                deleted_stone += 1
+        delete_stone_count = 0 # 삭제할 돌의 갯수
+        for rock in rocks:
+            if rock - prev_stone < mid: # 둘 사이의 거리가 mid값보다 작다면 삭제 대상 (최솟값중 최댓값을 구해야 한다..)
+                delete_stone_count += 1
             else:
-                prev_stone = idx # 살아남은 돌
+                prev_stone = rock
+
+            if delete_stone_count > n:
+                break
+        if distance - prev_stone < mid:
+            delete_stone_count += 1
                 
-            if deleted_stone > n:
-                break # 돌이 개수만큼 제거됨
-        
-        if deleted_stone > n:
-            end = mid - 1 # 탐색 범위를 줄인다, 개수가 더 많이 제거된 경우
+        if delete_stone_count > n:
+            end = mid - 1
         else:
             answer = mid
             start = mid + 1
+            
+        
+        
     return answer
