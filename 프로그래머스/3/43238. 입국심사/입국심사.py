0fd7addfe69b3524a00d25e092b20844769b9151
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-06 19:35:28 +0900

    [level 3] Title: 입국심사, Time: 385.22 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -2,17 +2,16 @@ def solution(n, times):
     answer = 0
     times.sort()
     start = 1
-    end = times[-1] * n
+    end = times[-1] * n  # 최댓값
     
     while start <= end:
         mid = (start + end) // 2
-        total = 0
+        count = 0
         for i in times:
-            total += (mid // i)
-        if total >= n: # 모든 사람이 심사 가능함(필요 이상의 인원이 심사 가능한 경우)
-            end = mid - 1
+            count += mid // i # 평가받을 수 있는 닝겐의 수
+        if count >= n: # 더 많이 받고있다면 범위를 줄여야지.
             answer = mid
+            end = mid - 1
         else:
-            start = mid + 1
-            
+            start = mid + 1 
     return answer
