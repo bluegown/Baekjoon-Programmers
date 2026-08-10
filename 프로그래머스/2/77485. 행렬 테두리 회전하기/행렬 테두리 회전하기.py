d9895b51ea1f9a5eb34f0425c600534e36bf561b
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-10 23:29:13 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 47.22 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,31 +1,38 @@
-# 22:51 시작..오늘안에 풀이 가능하려나
-import copy
 def solution(rows, columns, queries):
     answer = []
-    graph = []
-    for i in range(rows):
-        app = [i for i in range((i * columns) + 1, (i * columns) + columns + 1)]
-        graph.append(app)
     
+    graph = [[0] * columns for _ in range(rows)]
+    cnt = 1
+    for i in range(rows):
+        for j in range(columns):
+            graph[i][j] = cnt
+            cnt += 1
+
     for i in queries:
-        
-        x1,y1,x2,y2 = i[0] - 1, i[1] - 1, i[2] - 1, i[3] - 1
-        min_value = graph[x1][y1]
-        first_value = graph[x1][y1]
-        # 왼쪽 위로 밀기
+        x1, y1, x2, y2 = i[0] - 1, i[1] - 1, i[2] - 1, i[3] - 1
+        prev = graph[x1][y1]
+        min_value = 10001
         for row in range(x1, x2):
-            graph[row][y1] = graph[row + 1][y1]
-            min_value = min(min_value , graph[row + 1][y1])
-        # 아래 왼쪽으로 밀기
+            graph[row][y1] = graph[row + 1][y1] # 위쪽으로 옮기기
+            min_value = min(min_value, graph[row][y1])
         for col in range(y1, y2):
-            graph[x2][col] = graph[x2][col + 1]
-            min_value = min(min_value , graph[x2][col + 1])
-        for row in range(x2 , x1, -1):
-            graph[row][y2] = graph[row-1][y2]
-            min_value = min(min_value , graph[row-1][y2])
-        for col in range(y2, y1 + 1, -1):
-            graph[x1][col] = graph[x1][col - 1]
-            min_value = min(min_value , graph[x1][col - 1])
-        graph[x1][y1 + 1] = first_value
+            graph[x2][col] = graph[x2][col + 1] # 왼쪽으로 옮기기
+            min_value = min(min_value, graph[x2][col])
+        for row in range(x2, x1, -1): 
+            graph[row][y2] = graph[row - 1][y2] # 아래로 옮기기
+            min_value = min(min_value, graph[row][y2])
+        for col in range(y2, y1 + 1, -1) :
+            graph[x1][col] = graph[x1][col-1]
+            min_value = min(min_value, graph[x1][col])
+        graph[x1][y1 + 1] = prev
+        min_value = min(min_value, prev)
         answer.append(min_value)
+        
+            
+            
+            
+            
+            
+        
+    
     return answer
