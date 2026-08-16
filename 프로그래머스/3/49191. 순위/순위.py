c308f0d99fe39a9caddd32a4942c9cf970da3de0
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-16 13:52:10 +0900

    [level 3] Title: 순위, Time: 73.45 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,13 +1,15 @@
 def solution(n, results):
     answer = 0
-    graph = [['X'] * (n) for _ in range(n)]
+    graph = [['X'] * n for _ in range(n)]
+    
     for i in range(n):
         for j in range(n):
-            if i == j:
+            if i == j :
                 graph[i][j] = 0
     for a,b in results:
         graph[a-1][b-1] = 1
         graph[b-1][a-1] = -1
+    
     for k in range(n):
         for a in range(n):
             for b in range(n):
@@ -15,6 +17,7 @@ def solution(n, results):
                     graph[a][b] = 1
                 if graph[a][k] == -1 and graph[k][b] == -1:
                     graph[a][b] = -1
+    
     for i in graph:
         if 'X' not in i:
             answer += 1
