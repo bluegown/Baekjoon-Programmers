c3111916a6b79d6e090051dc72fc63b05cb7c835
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-17 21:43:59 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,21 +1,22 @@
+        
 def solution(tickets):
     answer = []
     graph = dict()
-    for start, end in tickets:
-        if start not in graph:
-            graph[start] = []
-        graph[start].append(end)
-    for key in graph:
+    
+    for a,b in tickets:
+        if a not in graph:
+            graph[a] = []
+        graph[a].append(b)
+            
+    for key, value in graph.items():
         graph[key].sort(reverse = True)
     stack = ["ICN"]
     while stack:
         top = stack[-1]
-        if top in graph and graph[top]:
+        if top in graph and graph[top]: # graph[top]에 원소 존재 > 다음으로 갈 수 있는곳 존재
             stack.append(graph[top].pop())
-        else:
+        else: # 다음으로 갈 수 있는곳이 없네 이제..
             answer.append(stack.pop())
             
-        
-            
-                
     return answer[::-1]
+    
