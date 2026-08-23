c6ee04de9377de8c644d899c690e8d8214903740
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-23 22:32:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.20 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,29 @@
 from collections import deque
-def bfs(graph):
+def solution(maps):
+    answer = 0
+    
     queue = deque()
-    queue.append((0,0)) # 초기 시작점 넣는다.
+    queue.append((0,0))
+    
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
-    
+    graph = [[0] * len(maps[0]) for _ in range(len(maps))]
+    n , m = len(maps) , len(maps[0])
+    graph[0][0] = 1
     while queue:
         x,y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= len(graph) or ny >= len(graph[0]):
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
-            if graph[nx][ny] == 0:
+            if maps[nx][ny] == 0:
                 continue
-            if graph[nx][ny] == 1:
+            if maps[nx][ny] == 1 and graph[nx][ny] == 0: # 방문 가능한 곳이고 아직 미방문인 경우
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx,ny))
-        
-def solution(maps):
-    answer = 0
-    graph = maps
-    bfs(graph)
-    print(graph)
-    if graph[-1][-1] == 1:
+                
+            
+    if graph[n-1][m-1] == 0:
         return -1
-    
-    return graph[-1][-1]
+    return graph[n-1][m-1]
