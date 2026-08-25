dc5930558ef5d3cd61908ed532e7a762ce807812
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-25 23:48:02 +0900

    [level 1] Title: 모의고사, Time: 1.42 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,16 +1,19 @@
 def solution(answers):
-    answer = []
-    ans = [[1,2,3,4,5],[2, 1, 2, 3, 2, 4, 2, 5],[ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    score = [0] * 3
+    answer = [0] * 3
+    arr = [[1,2,3,4,5] , [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
     
-    for i in range(len(ans)):
-        for j in range(len(answers)):
-            if answers[j] == ans[i][j % len(ans[i])]:
-                score[i] += 1
-    
-    for i in range(len(score)):
-        if max(score) == score[i]:
-            answer.append(i+1)
+    for index , value in enumerate(answers):
+        if value == arr[0][index % len(arr[0])]:
+            answer[0] += 1
+        if value == arr[1][index % len(arr[1])]:
+            answer[1] += 1
+        if value == arr[2][index % len(arr[2])]:
+            answer[2] += 1
         
-    
-    return answer
+    arr = []
+    for i in range(len(answer)):
+        if answer[i] == max(answer):
+            arr.append(i + 1)
+        
+        
+    return arr
