9690de271f5240aa0de8dc80eacea3bc0605c043
이성현 <123540125+bluegown@users.noreply.github.com>
2026-08-28 22:12:13 +0900

    [level 2] Title: 피로도, Time: 19.07 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,17 @@
 from itertools import permutations
 def solution(k, dungeons):
-    answer = 0
-    arr = list(permutations(dungeons,len(dungeons)))
-    for i in arr: # 실행 횟수
-        now = k
-        ans = 0
-        for minValue ,value in i:
-            if now < minValue: # 현재 피가 최소 필요도보다 작은 경우
-                break # 더 할필요 없음
-            now -= value # 던전 피 까고
-            ans += 1
-        answer = max(answer, ans)
-        
-            
-        
+    answer = -1
+    # 현재 피로도가 k
+    arr = list(permutations(dungeons , len(dungeons)))
+    value_k = k
+    for i in arr:
+        count = 0
+        value_k = k
+        for j in i:
+            min_value , minus_value = j[0], j[1]
+            if value_k < min_value:
+                break
+            value_k -= minus_value
+            count += 1
+        answer = max(answer , count)
     return answer
