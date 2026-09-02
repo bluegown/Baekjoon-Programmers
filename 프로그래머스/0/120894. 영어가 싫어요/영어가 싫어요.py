5dac38f7d356b41f054608e546816141cf3f3308
이성현 <123540125+bluegown@users.noreply.github.com>
2026-09-02 20:40:58 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = ''
+    dic = {'zero': '0','one' : '1','two' : '2','three' :'3','four' : '4','five' : '5','six' : '6','seven' : '7','eight' : '8','nine' : '9'}
+    string = ''
+    for i in range(len(numbers)):
+        string += numbers[i]
+        if string in dic:
+            answer += dic[string]
+            string = ''
+        
+    
+    return int(answer)
