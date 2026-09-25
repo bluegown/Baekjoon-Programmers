7b2f2eee45b2144223354dde792cb7facb6bc76a
이성현 <123540125+bluegown@users.noreply.github.com>
2026-09-25 16:27:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.11 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,27 @@
 from collections import deque
 def solution(maps):
     answer = 0
-    
     queue = deque()
     queue.append((0,0))
-    
+    n, m = len(maps), len(maps[0])
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
-    graph = [[0] * len(maps[0]) for _ in range(len(maps))]
-    n , m = len(maps) , len(maps[0])
-    graph[0][0] = 1
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
+    visited[0][0] = True
     while queue:
-        x,y = queue.popleft()
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
-            if maps[nx][ny] == 0:
+            if maps[nx][ny] == 0: # 방문 불가능한 노드
                 continue
-            if maps[nx][ny] == 1 and graph[nx][ny] == 0: # 방문 가능한 곳이고 아직 미방문인 경우
-                graph[nx][ny] = graph[x][y] + 1
+            if not visited[nx][ny]:
+                maps[nx][ny] = maps[x][y] + 1
+                visited[nx][ny] = True
                 queue.append((nx,ny))
-                
             
-    if graph[n-1][m-1] == 0:
+    if not visited[-1][-1]:
         return -1
-    return graph[n-1][m-1]
+    return maps[-1][-1]
