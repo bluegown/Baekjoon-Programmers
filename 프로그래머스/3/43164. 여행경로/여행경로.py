36f10ea762de45daa3048b73c278d5810f7cfdb5
이성현 <123540125+bluegown@users.noreply.github.com>
2026-09-26 23:38:35 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,22 +1,22 @@
-        
+from collections import deque
 def solution(tickets):
     answer = []
     graph = dict()
-    
     for a,b in tickets:
         if a not in graph:
             graph[a] = []
         graph[a].append(b)
-            
     for key, value in graph.items():
         graph[key].sort(reverse = True)
     stack = ["ICN"]
     while stack:
         top = stack[-1]
-        if top in graph and graph[top]: # graph[top]에 원소 존재 > 다음으로 갈 수 있는곳 존재
+        if top in graph and graph[top]: # 다음으로 갈 원소가 있는 경우
             stack.append(graph[top].pop())
-        else: # 다음으로 갈 수 있는곳이 없네 이제..
-            answer.append(stack.pop())
+        else: # 이제 갈곳이 없다.. 순회가 끝난 경우라면?
+            answer.append(stack.pop())# 마지막에 거꾸로 담고
             
-    return answer[::-1]
-    
+            
+            
+        
+    return answer[::-1] # 다시 이걸 거꾸로 담는 행위이다.
