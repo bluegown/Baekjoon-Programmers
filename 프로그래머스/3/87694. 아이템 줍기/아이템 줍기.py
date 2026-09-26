0df329e832781c6b541c4e8321dd932ef1fae89f
이성현 <123540125+bluegown@users.noreply.github.com>
2026-09-26 23:05:21 +0900

    [level 3] Title: 아이템 줍기, Time: 0.94 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,31 +1,34 @@
 from collections import deque
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
-    graph = [[-1] * (102) for _ in range(102)]
-    visited = [[-1] * (102) for _ in range(102)]
-    for x in rectangle:
-        x1, y1, x2, y2 = x[0] * 2 , x[1] * 2, x[2] * 2, x[3] * 2
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
-                if x1 < i < x2 and y1 < j < y2: # 내부
-                    graph[i][j] = 0 
-                elif graph[i][j] != 0: # 테두리
+    graph = [[-1] * 102 for _ in range(102)] # - 1은 외부
+    distance = [[0] * 102 for _ in range(102)] 
+    visited = [[False] * 102 for _ in range(102)] # - 1은 외부
+    for left_x, left_y, right_x, right_y in rectangle:
+        left_x,left_y,right_x,right_y = left_x * 2, left_y * 2, right_x * 2, right_y * 2
+        for i in range(left_x , right_x + 1):
+            for j in range(left_y, right_y + 1):
+                if left_x < i < right_x and left_y < j < right_y:
+                    graph[i][j] = 0
+                elif graph[i][j] != 0:
                     graph[i][j] = 1
     queue = deque()
-    queue.append((characterX * 2,characterY * 2))
+    queue.append((characterX * 2, characterY * 2))
+    visited[characterX * 2][characterY * 2] = True
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
-    visited[characterX * 2][characterY * 2] = 0
     while queue:
         x,y = queue.popleft()
         if x == itemX * 2 and y == itemY * 2:
-            return visited[x][y] // 2 
+                return distance[x][y] // 2
         for i in range(4):
-            nx = x + dx[i]
+            nx = x + dx[i] 
             ny = y + dy[i]
-            if graph[nx][ny] == 1 and visited[nx][ny] == -1:
-                visited[nx][ny] = visited[x][y] + 1
+            
+            if not visited[nx][ny] and graph[nx][ny] == 1:
+                visited[nx][ny] = True
+                distance[nx][ny] = distance[x][y] + 1
                 queue.append((nx,ny))
+ 
         
-
-    return visited[itemX][itemY] // 2
+    return distance[itemX][itemY] // 2
